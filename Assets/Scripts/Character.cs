d3ff3b96ebfa00ae778e7261ccdc51eb90010fd4
Matthieu Gorce <matthieu.gorce@imerir.com>
2018-11-23 14:38:30 +0000

    Player and Monster
    Issue #2
    Create Coordinates, Entity and Character (class use for the player and the monster).
    TODO :
      -Character :
           - implements ICombat and IMovable
           - add spell

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Character extends Entity {
+	//TODO
+	//add implements ICombat and IMovable
+
+	private string name;
+	private Coordinates coord;
+	private bool isPlayable;
+	//private Spell mySpell[];
+	private int manaPoint;
+	private int lifePoint;
+
+	public Character (string name,Coordinates coord, bool isPlayable, int manaPoint, int lifePoint){
+		//TODO
+		//add spell array
+
+		this.name = name;
+		this.coord.setX(coord.getX());
+		this.coord.setY(coord.getY());
+		this.isPlayable = isPlayable;
+		this.manaPoint = manaPoint;
+		this.lifePoint = lifePoint;
+	}
+
+	public Character (string name,int x, int y, bool isPlayable, int manaPoint, int lifePoint){
+		//TODO
+		//add spell array
+
+		this.name = name;
+		this.coord.setX(x);
+		this.coord.setY(y);
+		this.isPlayable = isPlayable;
+		this.manaPoint = manaPoint;
+		this.lifePoint = lifePoint;
+	}
+}
---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Entity {
+	private Coordinates coord;
+
+	public Entity (Coordinates coord)
+	{
+		this.coord.setX(coord.getX());
+		this.coord.setY(coord.getY());
+	}
+
+	public Entity (int x, int y)
+	{
+		this.coord.setX(x);
+		this.coord.setY(y);
+	}
+}
